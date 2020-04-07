78a562292e284c1ee4b4754f73ecd49283c1bd73
KatharineWu <katharine.wuf@gmail.com>
2020-04-07 18:47:31 -0500

    ids of cards retained, displayed in console log

---
+++ gameLogic.js
@@ -0,0 +1,9 @@
+function revealCard(cardID) {
+console.log("clicked ", cardID);
+  var image = document.getElementById(cardID);
+  for (let i = 0; i < cardarray.length; i++) {
+    if (cardarray[i].name = cardID) {
+      image.src = cardarray[i].image;
+    }
+  }
+}
