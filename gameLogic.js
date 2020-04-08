f5db80f3bef505160b98e0c978e7a707ecbff99a
KatharineWu <katharine.wuf@gmail.com>
2020-04-07 19:40:38 -0500

    can flip back to backside of card upon second click

--- gameLogic.js
+++ gameLogic.js
@@ -2,9 +2,15 @@ function revealCard(cardID) {
   var image = document.getElementById(cardID);
   console.log(cardID);
   console.log(cardarray);
-  for (let i = 0; i < cardarray.length; i++) {
-    if (cardID.includes(cardarray[i].name)) {
-      image.src = cardarray[i].image;
+  if (image.src == "https://i.imgur.com/iSazVxA.png") {
+    for (let i = 0; i < cardarray.length; i++) {
+      if (cardID.includes(cardarray[i].name)) {
+        image.src = cardarray[i].image;
+      }
     }
+  }
+  else
+  {
+    image.src = "https://i.imgur.com/iSazVxA.png";
   }
 }
