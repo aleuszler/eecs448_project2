16f1ae80272e0ee86b0af58dc560a3b4549a8e46
KatharineWu <katharine.wuf@gmail.com>
2020-04-17 13:04:18 -0500

    fixed wrong cards flipping back over

--- displaycards.js
+++ displaycards.js
@@ -2,7 +2,7 @@ var cardAmt = 8;
 
 window.addEventListener("load", setupCards(cardAmt));
 
-var randomarray = [];
+// var randomarray = [];
 
 function getRandomItem() {//Pulls an card from arrayofcards.js
   var item = cardarray[Math.floor(Math.random() * cardarray.length)];
--- gameLogic.js
+++ gameLogic.js
@@ -1,7 +1,8 @@
 let card1IDHolder;
 let card2IDHolder;
 let matchCard=[];
-matchCard.length=8;
+var randomarray=[];
+matchCard.length=randomarray.length;
 function revealCard(cardID) {
   console.log(randomarray);
   var image = document.getElementById(cardID);
@@ -10,7 +11,7 @@ function revealCard(cardID) {
   if (
      image.src == "https://i.imgur.com/iSazVxA.png" //&&
     // !displayTwo(randomarray)
-  ) {
+    ) {
     for (let i = 0; i < cardarray.length; i++) {
       if (cardID.includes(cardarray[i].name)) {
         image.src = cardarray[i].image;
@@ -25,35 +26,25 @@ function revealCard(cardID) {
       card2IDHolder=image;
     }
     console.log("card1");
-    console.log(card1IDHolder);
+    console.log(card1IDHolder.src);
     console.log("card2");
-    console.log(card2IDHolder);
-    if(card1IDHolder!=undefined && card2IDHolder!=undefined && card1IDHolder.src==card2IDHolder.src)//if cards match execute
-    {
-      for(let i=0; i<8;i++)
-      {
-        if(matchCard[i]==undefined)//this function is not removing EventListener as it should *FIX*
-        {
-          matchCard[i]=card1IDHolder;
-          matchCard[i+1]=card2IDHolder;
-          matchCard[i].removeEventListener('click', revealCard);//removes onclick from matched cards
-          matchCard[i+1].removeEventListener('click', revealCard);//removes onclick from matched cards
-          card1IDHolder=undefined;//resets cardholder ID for next pair
-          card2IDHolder=undefined;//resets cardholder ID for next pair
-          console.log(matchCard[i]);
-          i=8;
-        }
+    console.log(card2IDHolder.src);
+    if (card1IDHolder != undefined && card2IDHolder != undefined) {
+      if(card1IDHolder.src!=card2IDHolder.src) { //after 2 seconds if the cards didn't match reflips them
+        setTimeout(() =>{
+          card1IDHolder.src="https://i.imgur.com/iSazVxA.png";
+          card2IDHolder.src="https://i.imgur.com/iSazVxA.png";
+          card1IDHolder=undefined;
+          card2IDHolder=undefined;
+        }, 1000);
       }
+      else {
+        card1IDHolder=undefined;
+        card2IDHolder=undefined;
+	  }
     }
-    else{//after 2 seconds if the cards didn't match reflips them
-      setTimeout(() =>{
-      card1IDHolder.src="https://i.imgur.com/iSazVxA.png";
-      card2IDHolder.src="https://i.imgur.com/iSazVxA.png";
-      card1IDHolder=undefined;
-      card2IDHolder=undefined;
-    }, 2000);
-    }
-  } else {
+  } 
+  else {
     if(card1IDHolder==image)//below if functions make IDholder for said image undefined if unselected lines 31 to 38
     {
       card1IDHolder=undefined;
